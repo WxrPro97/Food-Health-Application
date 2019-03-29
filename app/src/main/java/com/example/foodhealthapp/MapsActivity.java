7753ba178518ea9bd1489075c29149e0782717be
darragh1612 <x17469074@student.ncirl.ie>
2019-03-29 18:52:49 +0000

    Custom marker

--- app/src/main/java/com/example/foodhealthapp/MapsActivity.java
+++ app/src/main/java/com/example/foodhealthapp/MapsActivity.java
@@ -12,6 +12,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -51,7 +52,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onLocationChanged(Location location) {
                 // Add a marker in Sydney and move the camera
                 LatLng sydney = new LatLng(-34, 151);
-                mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney").icon(BitmapDescriptorFactory.fromResource(R.drawable.map_marker)));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
             }
 
