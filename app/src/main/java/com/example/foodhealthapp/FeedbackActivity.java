3ebcfd5d6f9876b7bb0b294ce58425de9f7ce484
Your Name <sharmahemantoh@gmail.com>
2019-04-11 17:54:45 +0100

    Comments

--- app/src/main/java/com/example/foodhealthapp/FeedbackActivity.java
+++ app/src/main/java/com/example/foodhealthapp/FeedbackActivity.java
@@ -10,6 +10,7 @@ import android.widget.EditText;
 
 public class FeedbackActivity extends AppCompatActivity {
 
+    //Two variables for edit text field to facilitate details
     private EditText subjectText;
     private EditText messageText;
 
@@ -18,22 +19,29 @@ public class FeedbackActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
 
+        //linking the variable to the edit text id's
         subjectText = findViewById(R.id.subject);
         messageText = findViewById(R.id.message);
 
+
         Button sendBtn = findViewById(R.id.send_button);
+        //action performed on click event through a listener
         sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Converts user input to string
                 String subject = subjectText.getText().toString();
                 String message = messageText.getText().toString();
 
+                //To fill the fields in the email client
                 Intent intent = new Intent(Intent.ACTION_SEND);
                 intent.putExtra(Intent.EXTRA_EMAIL, new String[] {""});
                 intent.putExtra(Intent.EXTRA_SUBJECT, subject);
                 intent.putExtra(Intent.EXTRA_TEXT, message);
 
+                //Filters only the email clients
                 intent.setType("message/rfc822");
+                //Shows the options menu
                 startActivity(Intent.createChooser(intent, "Choose email client!"));
             }
         });
