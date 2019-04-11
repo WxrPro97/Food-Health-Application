1789fac9a64ca3c167386df31418de39f49a9e7b
Your Name <sharmahemantoh@gmail.com>
2019-04-11 17:44:14 +0100

    Feedback page update

--- app/src/main/java/com/example/foodhealthapp/FeedbackActivity.java
+++ app/src/main/java/com/example/foodhealthapp/FeedbackActivity.java
@@ -2,13 +2,40 @@ package com.example.foodhealthapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FeedbackActivity extends AppCompatActivity {
+
+    private EditText subjectText;
+    private EditText messageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        subjectText = findViewById(R.id.subject);
+        messageText = findViewById(R.id.message);
+
+        Button sendBtn = findViewById(R.id.send_button);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String subject = subjectText.getText().toString();
+                String message = messageText.getText().toString();
+
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[] {""});
+                intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+                intent.putExtra(Intent.EXTRA_TEXT, message);
+
+                intent.setType("message/rfc822");
+                startActivity(Intent.createChooser(intent, "Choose email client!"));
+            }
+        });
     }
 }
